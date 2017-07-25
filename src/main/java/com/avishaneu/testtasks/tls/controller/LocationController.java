8dc272aee7c0dc565efaf23d3246939f890afd42
avishaneu <ultimate.theory.of.everything@gmail.com>
2017-07-25 08:24:39 +0300

    empty controllers for Locations and Routes

---
+++ src/main/java/com/avishaneu/testtasks/tls/controller/LocationController.java
@@ -0,0 +1,17 @@
+package com.avishaneu.testtasks.tls.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by avishaneu on 7/25/2017.
+ */
+@RestController
+@RequestMapping("/locations")
+public class LocationController {
+
+    private static final Logger log = LoggerFactory.getLogger(LocationController.class);
+
+}
